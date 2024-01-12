8b21e6e2539a80a3f838687f8a6cf5e2782960bd
Raafey khan <113880768+Raafey-Khan@users.noreply.github.com>
2024-01-12 22:44:08 +0530

    Update board.js

--- (Kanban Board)/fullstack-kanban-app/server/src/v1/controllers/board.js
+++ (Kanban Board)/fullstack-kanban-app/server/src/v1/controllers/board.js
@@ -2,6 +2,7 @@ const Board = require('../models/board')
 const Section = require('../models/section')
 const Task = require('../models/task')
 
+
 exports.create = async (req, res) => {
   try {
     const boardsCount = await Board.find().count()
